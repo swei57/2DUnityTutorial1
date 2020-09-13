eec1cd9bb3207160cb36e90ede4300881772a623
song wei <swei57@uwo.ca>
2020-09-13 16:55:52 -0400

    init

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Vector2 targetPosition;
+    public float yIncrement;
+    public float speed;
+    public float maxHeight;
+    public float minHeight;
+    public int health = 5;
+    private void Update()
+    {
+    //moving the player vertically
+    //Time.deltaTime makes sure that movement is consistent in all PCs
+    transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+     if(Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHeight){
+         targetPosition = new Vector2(transform.position.x, transform.position.y + yIncrement);
+     }   else if(Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight){
+         targetPosition = new Vector2(transform.position.x, transform.position.y - yIncrement);
+     }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject obstable;
+    private float timeBetweenSpawn;
+    public float startTimeBetweenSpawn; 
+    public float decreaseTime;
+    public float minTime = 0.65f;
+
+    private void Update(){
+        if(timeBetweenSpawn <= 0){
+            if(obstable != null){
+            Instantiate(obstable, transform.position, Quaternion.identity);
+            }
+            timeBetweenSpawn = startTimeBetweenSpawn;
+            if(startTimeBetweenSpawn > minTime){
+             startTimeBetweenSpawn -= decreaseTime;
+            }
+        }else{
+            timeBetweenSpawn -= Time.deltaTime;
+        }
+    }
+
+}
